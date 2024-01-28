c6087d9f0ac9c4d8d386291e5288cba67fed6082
iamthepoe <lucagamerbrime@gmail.com>
2024-01-28 16:17:24 -0300

    feat: add POST heroes and GET heroes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
 import express from 'express'
+import redis from 'redis';
+import { setupDatabase, client } from './database/client.js';
+import { validateHero } from './model/hero.js';
 
 const app = new express();
+const cache = redis.createClient();
 
-app.listen(3000, ()=>{
-    console.log('Server is running at http://localhost:3000');
+app.use(express.json());
+
+app.get('/heroes', async (_, res) => {
+    const cachedHeroes = await cache.get('heroes');
+
+    if(cachedHeroes){
+        return res.send(JSON.parse(cachedHeroes));
+    }
+
+    const heroes = await client('heroes').select();
+
+    cache.set('heroes', JSON.stringify(heroes), {EX: 10});
+    res.send(heroes);
 });
+
+app.post('/heroes', async (req, res) => {
+    const { name, powerLevel } = req.body;
+    const validation = validateHero({ name, powerLevel });
+    
+    if(!validation.ok){
+        return res.status(400).json(validation);
+    }
+
+    const heroId = (await client('heroes').insert({ name, powerLevel }, 'id'))[0].id;
+    res.send({ id: heroId });
+});
+
+const initServer = async () =>{
+    await setupDatabase();
+    await cache.connect();
+
+    app.listen(3000, ()=>{
+        console.log('Server is running at http://localhost:3000');
+    });   
+}
+
+
+initServer();
